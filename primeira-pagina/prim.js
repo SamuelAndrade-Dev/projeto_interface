c2d7fddc56680d51264f4959a62d023c2c78abff
Samuel Andrade <samuelandrade.spa@gmail.com>
2025-05-31 22:58:28 -0300

    form-pag-search

--- primeira-pagina/prim.js
+++ primeira-pagina/prim.js
@@ -2,6 +2,8 @@ const slides = document.querySelector('.container-imagens');
 const images = document.querySelectorAll('.slider');
 const prevBtn = document.getElementById('prev-button');
 const nextBtn = document.getElementById('next-button');
+const carrossel = document.getElementById('container-carrossel');
+const scrollAmount = 300;
 
 let index = 0;
 
@@ -18,3 +20,10 @@ prevBtn.addEventListener('click', () => {
     index = (index - 1 + images.length) % images.length;
     updateSlider();
 });
+
+function scrollCarrossel(direction) {
+    carrossel.scrollBy({
+        left: scrollAmount * direction,
+        behavior: 'smooth'
+    });
+} 
